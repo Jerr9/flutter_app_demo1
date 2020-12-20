6d54d47aada974697842599efbf5b3ed6cc582fa
MSI-GS63\jerry <806242204@qq.com>
2020-12-20 15:30:17 +0800

    'use_statefulWidget'

--- lib/main.dart
+++ lib/main.dart
@@ -10,16 +10,7 @@ void main() {
             "HomePageTitle"
           ),
         ),
-        body: Center(
-          child: Text(
-            "Hello world",
-            textDirection: TextDirection.ltr, // ltr: left to right, rtl: right to left
-            style: TextStyle(
-              color: Colors.yellow,
-              fontSize: 22
-            )
-          )
-        ),
+        body: MyStatefulContent(),
         floatingActionButton: ButtonBar(
           buttonTextTheme: ButtonTextTheme.primary,
           children: [
@@ -33,3 +24,74 @@ void main() {
   );
 }
 
+class MyStatefulContent extends StatefulWidget {
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return MyStatefulContentState();
+  }
+}
+
+class MyStatefulContentState extends State<MyStatefulContent> {
+  int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Center(
+        child: Column(
+            children: [
+              Text("当前计数：${this.count}", style: TextStyle(fontSize: 30)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  RaisedButton(
+                    onPressed: () {
+                      setState((){
+                        count--;
+                      });
+                    },
+                    textTheme: ButtonTextTheme.accent,
+                    child: Text("点击-", style: TextStyle(color: Colors.white)),
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      setState((){
+                        count++;
+                      });
+                    },
+                    textTheme: ButtonTextTheme.accent,
+                    child: Text("点击+", style: TextStyle(color: Colors.white)),
+                  )
+                ],
+              )
+            ]
+        )
+    );
+  }
+}
+
+
+// 不能实现
+class MyStatelessContent extends StatelessWidget {
+  final int count = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+          children: [
+            Text("当前计数：${count}", style: TextStyle(fontSize: 30)),
+            RaisedButton(
+              onPressed: () => {
+                // count++;
+              },
+              textTheme: ButtonTextTheme.accent,
+              child: Text("点击", style: TextStyle(color: Colors.white)),
+            )
+          ]
+      )
+    );
+  }
+}
