b03a7c54554a8c248fbf06deb6bd24c363d796da
MSI-GS63\jerry <806242204@qq.com>
2020-12-20 17:49:51 +0800

    'lifecycle'

--- lib/main.dart
+++ lib/main.dart
@@ -10,26 +10,71 @@ void main() {
             "HomePageTitle"
           ),
         ),
-        body: Center(
-          child: Text(
-            "Hello world",
-            textDirection: TextDirection.ltr, // ltr: left to right, rtl: right to left
-            style: TextStyle(
-              color: Colors.yellow,
-              fontSize: 22
-            )
-          )
-        ),
-        floatingActionButton: ButtonBar(
-          buttonTextTheme: ButtonTextTheme.primary,
-          children: [
-            Text(
-              "Button"
-            )
-          ]
-        )
+        body: PageContainer(),
       )
     )
   );
+}
+
+class PageContainer extends StatefulWidget {
+  PageContainer() {
+    print("PageContainer 构造方法");
+  }
+  @override
+  State<PageContainer> createState() {
+    print("PageContainer createState方法");
+    return PageContainerState();
+  }
+}
+
+class PageContainerState extends State<PageContainer> {
+  PageContainerState() {
+    print("PageContainerState 构造方法");
+  }
+  @override
+
+  int value = 1;
+
+  @override
+  void initState() {
+    print("PageContainerState initState方法");
+  }
+
+  @override
+  void didChangeDependencies() {
+    print("PageContainerState didChangeDependencies方法");
+  }
+
+  @override
+  void didUpdateWidget(PageContainer oldWidget) {
+    print("PageContainerState didUpdateWidget方法");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("PageContainerState build方法");
+    return  Center(
+        child: Column(
+          children: [
+            Text(
+                "Hello world $value",
+                textDirection: TextDirection.ltr, // ltr: left to right, rtl: right to left
+                style: TextStyle(
+                    color: Colors.yellow,
+                    fontSize: 22
+                )
+            ),
+            RaisedButton(
+              child: Text("点我"),
+              onPressed: () {
+                setState(() {
+                  value++;
+                });
+              }
+            )
+          ],
+        )
+    );
+  }
 }
 
